5f4e6674b0a7b9119d3233bdc8d2e005d82c813c
Satoshi Kobayashi <satoshi.forest@gmail.com>
2023-04-27 23:21:41 +0900

    add some field to Todo

--- ent/schema/todo.go
+++ ent/schema/todo.go
@@ -1,6 +1,11 @@
 package schema
 
-import "entgo.io/ent"
+import (
+	"time"
+
+	"entgo.io/ent"
+	"entgo.io/ent/schema/field"
+)
 
 // Todo holds the schema definition for the Todo entity.
 type Todo struct {
@@ -9,7 +14,17 @@ type Todo struct {
 
 // Fields of the Todo.
 func (Todo) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.Text("text").NotEmpty(),
+		field.Time("created_at").Default(time.Now()).Immutable(),
+		field.Enum("status").
+			NamedValues(
+				"inProgress", "IN_PROGRESS",
+				"Completed", "COMPLETED",
+			).
+			Default("IN_PROGRESS"),
+		field.Int("priority").Default(0),
+	}
 }
 
 // Edges of the Todo.
